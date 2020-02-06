ad72bb0ef8ce7bffada87d5af4c335252f9cb939
vemarav <aravind@kiprosh.com>
2020-02-06 21:30:14 +0530

    mobile: navigation

--- app/App.tsx
+++ app/App.tsx
@@ -1,48 +1,34 @@
-import * as React from 'react'
-import {
-  Animated,
-  Dimensions,
-  Text,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native'
+import React from 'react'
+import {View, Text, Button} from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
 
-const {height} = Dimensions.get('window')
-
-const AnimatedView = Animated.createAnimatedComponent(View)
-
-const App: React.FC<any> = (props: any) => {
-  const {} = props
-  const opacity: Animated.Value = new Animated.Value(1)
-
-  const animate = () => {
-    Animated.sequence([
-      Animated.timing(opacity, {
-        toValue: 0.4,
-        duration: 250,
-      }),
-      Animated.timing(opacity, {
-        toValue: 1,
-        duration: 250,
-      }),
-    ]).start()
-  }
-
+function HomeScreen(props: {navigation: any}) {
   return (
-    <View style={{height: height}}>
-      <TouchableWithoutFeedback onPress={animate}>
-        <AnimatedView
-          style={{
-            height,
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            opacity: opacity,
-          }}>
-          <Text>Hello, World!!</Text>
-        </AnimatedView>
-      </TouchableWithoutFeedback>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button title="Go About" onPress={() => props.navigation.push('About')} />
     </View>
+  )
+}
+
+function AboutScreen(props: {navigation: any}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button title="Go Home" onPress={() => props.navigation.pop()} />
+    </View>
+  )
+}
+
+const Stack = createStackNavigator()
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import App from './app/App';
+import {AppRegistry} from 'react-native'
+import {name as appName} from './app.json'
+import App from './app/App'
+import 'react-native-gesture-handler'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
